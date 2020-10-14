9ea1b147d8f5202d501c009e1474eceeb5ff1cbf
Alan Stone <alderocas@netc.eu>
2020-10-14 14:55:15 +0200

    - scrape from tests/files/ for now (comment out 'start_urls', add file paths). - add "_parse_description", "_parse_start". - working on "_parse_location".

--- city_scrapers/spiders/chi_northeastern_il_university.py
+++ city_scrapers/spiders/chi_northeastern_il_university.py
@@ -1,16 +1,20 @@
-from city_scrapers_core.constants import NOT_CLASSIFIED
+import os
+from datetime import datetime, time
+
+from city_scrapers_core.constants import BOARD, COMMITTEE
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
-from datetime import datetime
 
 
 class ChiNortheasternIlUniversitySpider(CityScrapersSpider):
     name = "chi_northeastern_il_university"
     agency = "Northeastern Illinois University"
     timezone = "America/Chicago"
-    #start_urls = ["https://www.neiu.edu/about/board-of-trustees/calendar-of-meetings", "https://www.neiu.edu/about/board-of-trustees/board-meeting-materials"]
-    start_urls = ['tests/files/chi_northeastern_il_university_calendar.html', 'tests/files/chi_northeastern_il_university_meeting_materials.html']
-    title_regex = '<li>.*?(?P<title>(Academic|Audit|Executive|Finance|Ad\\ hoc).*?(Committee)).*?<\\/li>'
+    #start_urls = ["https://www.neiu.edu/about/board-of-trustees/calendar-of-meetings", "https://www.neiu.edu/about/board-of-trustees/board-meeting-materials",]
+    start_urls = [f"file://{os.getcwd()}/tests/files/chi_northeastern_il_university_calendar.html"]
+
+    title_regex = "<li>.*?(?P<title>(Academic|Audit|Executive|Finance|Ad\\ hoc).*?(Committee)).*?<\\/li>"
+    desc_regex = "<li>.*?(?P<title>(Academic|Audit|Executive|Board|Finance|Special|Ad\\ hoc|All).*?((Committee.*Meeting)|Committee|Meeting)).*?<\\/li>"
 
     def parse(self, response):
         """
@@ -21,18 +25,20 @@ class ChiNortheasternIlUniversitySpider(CityScrapersSpider):
         """
 
         for annum in response.css("section.page-content ul"):
-            year_str = annum.xpath('preceding-sibling::h2[1]').re_first('(?P<year>\\d{4}) Board Meetings')
+            year_str = annum.xpath("preceding-sibling::h2[1]").re_first(
+                "(?P<year>\\d{4}) Board Meetings"
+            )
 
             for item in annum.css("li"):
                 meeting = Meeting(
-                    title=self._parse_title(item),
-                    description="",
                     classification=self._parse_classification(item),
+                    title=self._parse_title(item),
+                    description=self._parse_description(item),
                     start=self._parse_start(item, year_str),
                     end=self._parse_end(item),
                     all_day=self._parse_all_day(item),
                     time_notes=self._parse_time_notes(item),
-                    location=self._parse_location(item),
+                    location=self._parse_location(item, year_str),
                     links=self._parse_links(item),
                     source=self._parse_source(response),
                 )
@@ -42,17 +48,30 @@ class ChiNortheasternIlUniversitySpider(CityScrapersSpider):
 
                 yield meeting
 
+    def _parse_classification(self, item):
+        if "Meeting" in item.get():
+            return BOARD
+        else:
+            return COMMITTEE
+
     def _parse_title(self, item):
         """Parse or generate meeting title."""
-        if 'Meeting' in item.get():
+        if "Meeting" in item.get():
             return "Board of Trustees"
         else:
-            return item.re_first(title_regex).replace('\xa0', ' ')
+            return item.re_first(self.title_regex).replace("\xa0", " ")
 
-    def _parse_start(self, item):
+    def _parse_description(self, item):
+        return item.re_first(self.desc_regex).replace("\xa0", " ")
+
+    def _parse_start(self, item, year):
         """Parse start datetime as a naive datetime object."""
-        date_str = item.re_first('.*day, (?P<date>.*?\d{1,2}).*')
-        return datetime.parser(f"{year_str} {date_str}", "%Y %B %d")
+        date_str = item.re(".*day(,)? (?P<date>.*?\\d{1,2}).*")[-1]
+        date = datetime.strptime(f"{year} {date_str}", "%Y %B %d")
+        if "Meeting" in item.get():
+            return datetime.combine(date, time(13))
+        else:
+            return datetime.combine(date, time(8, 30))
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
@@ -66,12 +85,18 @@ class ChiNortheasternIlUniversitySpider(CityScrapersSpider):
         """Parse or generate all-day status. Defaults to False."""
         return False
 
-    def _parse_location(self, item):
+    def _parse_location(self, item, year):
         """Parse or generate location."""
-        return {
-            "address": "",
-            "name": "",
-        }
+        if year == "2018" and item.re_first("Thursday, September 20\\*"):
+            return {
+                "address": "700 E. Oakwood Blvd., Chicago, Ill., 60653",
+                "name": "Carruthers Center for Inner City Studies",
+            }
+        else:
+            return {
+                "address": "",
+                "name": "",
+            }
 
     def _parse_links(self, item):
         """Parse or generate links."""
